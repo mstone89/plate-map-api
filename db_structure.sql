eb176166e308ae9d08ecd982bab2b8b39f389d4a
Molly Stone <molly.stone89@gmail.com>
2019-06-21 11:54:16 -0400

    Update seed data with cell count

--- db_structure.sql
+++ db_structure.sql
@@ -2,6 +2,6 @@
 CREATE TABLE plates (id SERIAL, name VARCHAR(50), samples INT, sc_reps INT, replicates INT, dilutions INT, cellcount INT);
 
 -- Seed Lists
-INSERT INTO plates (name, samples, sc_reps, replicates, dilutions, cellcount) VALUES ('Plate 1', 12, 3, 2, 3);
-INSERT INTO plates (name, samples, sc_reps, replicates, dilutions, cellcount) VALUES ('Plate 2', 8, 3, 3, 3);
-INSERT INTO plates (name, samples, sc_reps, replicates, dilutions, cellcount) VALUES ('Plate 3', 9, 3, 4, 2);
+INSERT INTO plates (name, samples, sc_reps, replicates, dilutions, cellcount) VALUES ('Plate 1', 12, 3, 2, 3, 96);
+INSERT INTO plates (name, samples, sc_reps, replicates, dilutions, cellcount) VALUES ('Plate 2', 8, 3, 3, 3, 96);
+INSERT INTO plates (name, samples, sc_reps, replicates, dilutions, cellcount) VALUES ('Plate 3', 9, 3, 4, 2, 96);
